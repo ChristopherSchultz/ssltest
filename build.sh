c0d0e7160f2b70c0e9bb6e3e5bd83866959d2669
Christopher Schultz <chris@christopherschultz.net>
2020-08-06 11:44:08 -0400

    Fail properly if javac fails.

--- build.sh
+++ build.sh
@@ -6,6 +6,15 @@ mkdir -p "${BASE}/build/classes"
 
 javac -Xlint:-options -source 1.5 -target 1.5 -d "${BASE}/build/classes" $( find "${BASE}"/src/java -name "*.java" )
 
+result=$?
+
+if [ "0" -ne "$result" ] ; then
+  echo
+  echo "Compile failed (exit code $result). See output above."
+
+  exit
+fi
+
 echo "Main-Class: net.christopherschultz.ssltest.SSLTest" > "${BASE}"/build/MANIFEST.MF
 
 jar cfm "${BASE}/build/ssltest.jar" "${BASE}/build/MANIFEST.MF" -C "${BASE}/build/classes" .
